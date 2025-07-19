9e1c942e9950a8f8b42ff9aa3f5176821511eb57
Mr64Z <mr.cy807@gmail.com>
2025-07-19 15:24:59 +0630

    Fix MiniPOS User Role Persistence Issue

--- config/database.js
+++ config/database.js
@@ -118,12 +118,6 @@ const initialize = () => {
 
       // Insert default admin user
       db.run(`
-        INSERT OR REPLACE INTO users (id, name, username, email, password, role, is_active) 
-        VALUES 
-          (1, 'Administrator', 'admin', 'admin@minipos.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 1),
-          (2, 'Aung Thu Myint', 'aungthumyint', 'aungthumyint@minipos.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'cashier', 1)
-      `);
-      db.run(`
         INSERT OR IGNORE INTO users (id, name, username, email, password, role, is_active) 
         VALUES 
           (1, 'Administrator', 'admin', 'admin@minipos.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 1),
